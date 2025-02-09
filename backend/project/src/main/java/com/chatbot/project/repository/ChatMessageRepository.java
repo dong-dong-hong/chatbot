d52152a11ef79f7550355364df168abce5944446
DongDong <sdh5518@naver.com>
2025-02-09 21:59:41 +0900

    챗봇 페이지 수정 기능 구현중

---
+++ backend/project/src/main/java/com/chatbot/project/repository/ChatMessageRepository.java
@@ -0,0 +1,9 @@
+package com.chatbot.project.repository;
+
+import com.chatbot.project.entity.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+}
